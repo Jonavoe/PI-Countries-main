286f9759ef142f8a2659ca968353c3998fc49aa6
Jonavoe <voeffray.jonathan@gmail.com>
2023-04-12 11:45:33 -0300

    Update getCountriesByName.js

--- api/src/controllers/getCountriesByName.js
+++ api/src/controllers/getCountriesByName.js
@@ -1,38 +1,20 @@
 const { default: axios } = require('axios');
 
 const getCountriesByName = async (name) => {
-		const url = `https://restcountries.com/v3.1/name/${name}`;
-		return await axios
-		.get(url)
-		.then((response) => {
-			const countries = response.data.filter((country) => {
-				return country.name.includes(name);
-			});
-			if (countries.length === 0) {
-				 			throw new Error(`No se encontraron países con el nombre "${name}"`);
+	const url = `https://restcountries.com/v3.1/name/${name}`;
+	return await axios.get(url).then((response) => {
+		const countries = response.data.filter((country) => {
+			return (
+				country.name.common.toLowerCase().includes(name) ||
+				country.name.official.toLowerCase().includes(name)
+			);
+		});
+		if (countries.length === 0) {
+			throw new Error(`No se encontraron países con el nombre "${name}"`);
 		} else {
-			return countries
+			return countries;
 		}
-})};
+	});
+};
 
 module.exports = { getCountriesByName };
-
-
-// name = name.toLowerCase(); // convertir a minúsculas
-// 	const url = `https://restcountries.com/v3.1/name/${name}`;
-// 	return await axios.get(url).then((response) => {
-// 		const countries = response.data.filter((country) => {
-// 			return (
-// 				country.name.common.toLowerCase().includes(name) || // buscar por nombre común
-// 				country.name.official.toLowerCase().includes(name) || // buscar por nombre oficial
-// 				Object.values(country.name.native).some((n) =>
-// 					n.common.toLowerCase().includes(name)
-// 				) // buscar por nombres nativos
-// 			);
-// 		});
-// 		if (countries.length === 0) {
-// 			throw new Error(`No se encontraron países con el nombre "${name}"`);
-// 		} else {
-// 			return countries;
-// 		}
-// 	});
