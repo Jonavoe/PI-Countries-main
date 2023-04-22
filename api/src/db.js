d7fd43d7dea60db5e35612c5838cd4d3798e05d5
Jonavoe <voeffray.jonathan@gmail.com>
2023-04-22 13:01:52 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_BDD } = process.env;
 const countryFunction = require('./models/Country');
 const activityFunction = require('./models/Activity');
 
